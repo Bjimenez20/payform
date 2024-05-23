baf0480b78b94d653dfaf9e769dd896b163f24e8
Bjimenez20 <112726778+Bjimenez20@users.noreply.github.com>
2024-05-23 14:38:35 +0000

    feat: agregar favicon

--- resources/views/layouts/dashboard/components/head.blade.php
+++ resources/views/layouts/dashboard/components/head.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="shortcut icon" href="" />
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico') }}"/>
 
     <link rel="canonical" href="https://demo-basic.adminkit.io/pages-blank.html" />
 
