8b3c19167525a9ac67c81a470f35703ac44e48b6
root <root@ip-172-31-16-60.ec2.internal>
2024-05-23 14:05:40 +0000

    feat: cambios prod

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '099e973c4ed32afb04daa6320fbff5eea9943772',
+        'reference' => '8c3702461e4e8953893eb7a9660d39915d9ecd05',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '099e973c4ed32afb04daa6320fbff5eea9943772',
+            'reference' => '8c3702461e4e8953893eb7a9660d39915d9ecd05',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
